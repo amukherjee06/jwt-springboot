07ac1ad878147b97b91f82c74ca598b355140e6d
Arnab Mukherjee <mukherjeearnab06@gmail.com>
2022-01-19 15:33:22 +0530

    02.A Simple hardcoded Spring security implementation

---
+++ jwt-demo/src/main/java/com/arnab/springboot/jwt/service/UserService.java
@@ -0,0 +1,24 @@
+package com.arnab.springboot.jwt.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		//Logic to get the user from DB
+		//In this case creating a dummy user
+		return new User("admin","admin@123",new ArrayList<>());
+	}
+	
+	
+
+}
